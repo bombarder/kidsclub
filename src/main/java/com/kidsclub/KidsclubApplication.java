a7d783f96482fea8ca54b02384bf47a8a0759f8c
Nick <makarovmykola@gmail.com>
2016-09-30 14:10:15 +0300

    adding gitignore

---
+++ src/main/java/com/kidsclub/KidsclubApplication.java
@@ -0,0 +1,12 @@
+package com.kidsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsclubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kidsclub/KidsclubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kidsclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KidsclubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
