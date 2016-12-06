169dd5ddcc53c18409a131261a2a09fd190db37c
nmakarov <nmakarov@playtika.com>
2016-12-06 16:07:46 +0200

    index.jsp added

--- src/main/java/com/kidsclub/controller/CustomerController.java
+++ src/main/java/com/kidsclub/controller/CustomerController.java
@@ -6,7 +6,6 @@ import com.kidsclub.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("/customer")
 public class CustomerController {
 
--- src/main/java/com/kidsclub/controller/HomeController.java
+++ src/main/java/com/kidsclub/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.kidsclub.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String index() {
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+    private Random random = new Random();
+
+    @GetMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        model.put("random", random.nextInt());
         return "index";
     }
 }
