d73a391a6597b00a0a3d3a6655f0ed7bc473c1f9
nmakarov <nmakarov@playtika.com>
2016-12-06 15:04:20 +0200

    React removed

--- src/main/java/com/kidsclub/configuration/KidsClubApplication.java
+++ src/main/java/com/kidsclub/configuration/KidsClubApplication.java
@@ -1,20 +1,23 @@
 package com.kidsclub.configuration;
 
-import com.kidsclub.service.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.kidsclub")
 @EnableJpaRepositories(basePackages = "com.kidsclub.repository")
 @EntityScan(basePackages = "com.kidsclub.model")
-public class KidsClubApplication {
+public class KidsClubApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(KidsClubApplication.class, args);
-		CustomerService bean = context.getBean(CustomerService.class);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KidsClubApplication.class);
+	}
 
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(KidsClubApplication.class, args);
 	}
 }
