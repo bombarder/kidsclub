d77a797e28eb1fd22d53c05623c827ff9d73a1f2
nmakarov <nmakarov@playtika.com>
2016-12-16 17:50:45 +0200

    Food list added

--- src/main/java/com/kidsclub/model/Order.java
+++ src/main/java/com/kidsclub/model/Order.java
@@ -16,6 +16,16 @@ public class Order extends AbstractPersistable<Long> {
     private Customer customer;
     @ManyToMany
     private List<Entertainment> entertainments;
+    @ManyToMany
+    private List<Food> food;
+
+    public List<Food> getFood() {
+        return food;
+    }
+
+    public void setFood(List<Food> food) {
+        this.food = food;
+    }
 
     public List<Entertainment> getEntertainments() {
         return entertainments;
